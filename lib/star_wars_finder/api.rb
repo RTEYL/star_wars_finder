ebdd77e9d95ecfb561b10e5c7fbb05996ec78271
RTEYL <tyler-w-jones@hotmail.com>
2020-03-23 18:59:15 -0700

    CLI static setup

--- lib/star_wars_finder/api.rb
+++ lib/star_wars_finder/api.rb
@@ -1,3 +1,9 @@
 class API
-
+    def self.list_categories
+        puts "1. people"
+        puts "2. planets"
+        puts "3. species"
+        puts "4. starships"
+        puts "5. vehicles \n\n"
+    end
 end
--- lib/star_wars_finder/cli.rb
+++ lib/star_wars_finder/cli.rb
@@ -1,3 +1,16 @@
 class CLI
-    
+    def start
+        self.welcome
+        self.get_categories
+    end
+    def welcome
+        puts "Welcome to my Star Wars API! \n\n"
+        sleep(1.5)
+        puts "You will be able to choose a category to search by \nand recieve information on your search topic \n\n"
+        sleep(1.5)
+    end
+    def get_categories
+        API.list_categories
+        puts "Select a category(number) to search by"
+    end
 end
