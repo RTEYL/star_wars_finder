98f0f69c27a4cb55c9051c503b06e78f1127e4e2
RTEYL <tyler-w-jones@hotmail.com>
2020-03-25 15:20:01 -0700

    update cli methods

--- lib/star_wars_finder/cli.rb
+++ lib/star_wars_finder/cli.rb
@@ -1,21 +1,14 @@
 class CLI
     def start
         self.welcome
-        self.display_categories
+        self.display_characters
     end
     def welcome
         puts "Welcome to my Star Wars Gem! \n\n"
-        sleep(1.5)
         puts "You will be able to see a list of characters and some info about them"
-        sleep(1)
     end
-    def display_categories
-        API.all_categories.each.with_index(1) do |key, i|
-            if key.include?("people") || key.include?("planets") || key.include?("species")
-                puts"#{i}. #{key.first}"
-            end
-        end
-        puts "3. exit"
-        puts "\nSelect a category(number) to view a list of objects in that category"
+    def display_characters
+        Person.top_10
+        puts "select a character in this top 10 list or search for a character by name"
     end
 end
