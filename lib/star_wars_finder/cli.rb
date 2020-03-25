6506cb72eff993aa592c450a8ec6e913d4283738
RTEYL <tyler-w-jones@hotmail.com>
2020-03-25 15:26:12 -0700

    switch to class methods

--- lib/star_wars_finder/cli.rb
+++ lib/star_wars_finder/cli.rb
@@ -1,14 +1,16 @@
 class CLI
-    def start
+    def self.start
         self.welcome
         self.display_characters
     end
-    def welcome
+    def self.welcome
         puts "Welcome to my Star Wars Gem! \n\n"
         puts "You will be able to see a list of characters and some info about them"
     end
-    def display_characters
+    def self.display_characters
         Person.top_10
         puts "select a character in this top 10 list or search for a character by name"
+        input = gets.strip
+        Person.search_all(input)
     end
 end
