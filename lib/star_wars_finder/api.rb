7b484768ec0a102cfc8dcc2663f9accdc9a132a9
RTEYL <tyler-w-jones@hotmail.com>
2020-03-25 17:36:01 -0700

    speed up http request time

--- lib/star_wars_finder/api.rb
+++ lib/star_wars_finder/api.rb
@@ -3,10 +3,11 @@ class API
         self.get_people
     end
     def self.get_people
+        puts "\nLoading...\n\n"
         people = Array.new
         i = 1
         while i < 10 do
-            response = HTTParty.get("https://swapi.co/api/people/?page=#{i}")
+            response = HTTParty.get("http://swapi.co/api/people/?page=#{i}")
             response["results"].each{|obj| people << obj}
             i+=1
         end
