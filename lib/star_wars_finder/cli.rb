da3526dac639485844c82c601655842c8dd471a6
RTEYL <tyler-w-jones@hotmail.com>
2020-03-25 17:36:41 -0700

    adjust display output

--- lib/star_wars_finder/cli.rb
+++ lib/star_wars_finder/cli.rb
@@ -5,12 +5,17 @@ class CLI
     end
     def self.welcome
         puts "Welcome to my Star Wars Gem! \n\n"
-        puts "You will be able to see a list of characters and some info about them"
+        sleep(2)
+        puts "You will be able to see a list of characters and some info about them\n\n"
     end
     def self.display_characters
         Person.top_10
-        puts "select a character in this top 10 list or search for a character by name"
-        input = gets.strip
+        puts "\nselect a character in this top 10 list or search for a character by name"
+        puts "Type 'exit' to exit"
+        input = gets.strip.downcase
         Person.search_all(input)
+    end
+    def self.display_search_res(character)
+        puts "#{character}"
     end
 end
