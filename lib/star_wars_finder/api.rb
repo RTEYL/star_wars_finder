a4893edc04b74918ded9c14c9b2820cf00335389
RTEYL <tyler-w-jones@hotmail.com>
2020-04-02 12:34:08 -0700

    fix indentation

--- lib/star_wars_finder/api.rb
+++ lib/star_wars_finder/api.rb
@@ -1,30 +1,30 @@
 class API
-    def self.get_characters
-        puts "\nLoading...\n\n"
-        people = Array.new
-        i = 1
-        while i < 10 do
-            response = HTTParty.get("http://swapi.co/api/people/?page=#{i}")
-            response["results"].each{|obj| people << obj}
-            i+=1
-        end
-        self.create_people_objects(people)
-    end
-    def self.create_people_objects(people)
-        people.each {|hash| hash["name"] = Character.new(hash)}
-    end
-    def self.create_species(species)
-        if species.class == String
-            species.gsub!("https", "http")
-            response = HTTParty.get("#{species}")
-            response["name"] = Species.new(response)
-        end
-    end
-    def self.create_homeworld(planet)
-        if planet.class == String
-            planet.gsub!("https", "http")
-            response = HTTParty.get("#{planet}")
-            response["name"] = Planet.new(response)
-        end
-    end
+	def self.get_characters
+		puts "\nLoading...\n\n"
+		people = Array.new
+		i = 1
+		while i < 10 do
+			response = HTTParty.get("http://swapi.co/api/people/?page=#{i}")
+			response["results"].each{|obj| people << obj}
+			i+=1
+		end
+		self.create_people_objects(people)
+	end
+	def self.create_people_objects(people)
+		people.each {|hash| hash["name"] = Character.new(hash)}
+	end
+	def self.create_species(species)
+		if species.class == String
+			species.gsub!("https", "http")
+			response = HTTParty.get("#{species}")
+			response["name"] = Species.new(response)
+		end
+	end
+	def self.create_homeworld(planet)
+		if planet.class == String
+			planet.gsub!("https", "http")
+			response = HTTParty.get("#{planet}")
+			response["name"] = Planet.new(response)
+		end
+	end
 end
